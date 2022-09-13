8786dfc951022576f565dd190ae64254aef68b03
Alexey_Kulik <alexey.kulik@protofire.io>
2022-09-13 21:29:43 +0400

    update cid-checker

--- k8s/cronjob_cid-checker.tf
+++ k8s/cronjob_cid-checker.tf
@@ -19,7 +19,7 @@ resource "kubernetes_cron_job_v1" "cid_checker" {
           spec {
             container {
               name  = "cid-checker"
-              image = "protofire/cid-checker:0.1.1"
+              image = "protofire/cid-checker:0.1.3"
               env {
                 name = "mongoURL"
                 value_from {
