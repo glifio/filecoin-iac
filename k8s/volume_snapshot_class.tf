90b19bc86d03c0edb64e361781a8bbe223f06746
Uladzislau Muraveika <uladzislau.muraveika@protofire.io>
2022-09-20 15:13:03 +0300

    [feat] - add module for aws snapshots creation, add code for volume_snapshot_class creation

---
+++ k8s/volume_snapshot_class.tf
@@ -0,0 +1,14 @@
+# Links about Volume Snapshots:
+# https://kubernetes.io/docs/concepts/storage/volume-snapshot-classes
+# https://aws.amazon.com/blogs/containers/using-ebs-snapshots-for-persistent-storage-with-your-eks-cluster/
+resource "kubernetes_manifest" "volume_snapshot_class" {
+  manifest = {
+    "apiVersion" = "snapshot.storage.k8s.io/v1"
+    "kind"       = "VolumeSnapshotClass"
+    "metadata" = {
+      "name" = "ebs-csi-aws"
+    }
+    "driver"         = "ebs.csi.aws.com"
+    "deletionPolicy" = "Delete"
+  }
+}
---
+++ modules/cronjob_aws_snapshots_managment/variables.tf
@@ -0,0 +1,18 @@
+variable "volume_name_prefix" {
+  type = string
+  default = "vol-lotus"
+}
+
+variable "volume_name_postfix" {
+  type = string
+  default = "lotus-0"
+}
+
+variable "sts_name" {
+  type = string
+}
+
+variable "namespace" {
+  type = string
+}
+
