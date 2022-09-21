81f5c346454faea82e97c500b132248a2ea5c149
Alexey_Kulik <alexey.kulik@protofire.io>
2022-09-21 15:24:48 +0400

    added codebuild

--- aws/iam.tf
+++ aws/iam.tf
@@ -1,22 +1,22 @@
-resource "aws_iam_role" "codebuild_eks" {
-  name               = "${module.generator.prefix}-codebuild-eks"
-  description        = "${module.generator.prefix} codebuild eks role"
-  assume_role_policy = file("${path.module}/templates/roles/codebuild_eks_role.pol.tpl")
-
-  tags = merge(
-    {
-      "Name" = "${module.generator.prefix}-codebuild-eks"
-    },
-    module.generator.common_tags
-  )
-}
-
-resource "aws_iam_role_policy_attachment" "eks_policy" {
-  role       = aws_iam_role.eks_master.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-}
-
-resource "aws_iam_role_policy_attachment" "eks_controller_policy" {
-  role       = aws_iam_role.eks_master.name
-  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
-}
+#resource "aws_iam_role" "codebuild_eks" {
+#  name               = "${module.generator.prefix}-codebuild-eks"
+#  description        = "${module.generator.prefix} codebuild eks role"
+#  assume_role_policy = file("${path.module}/templates/roles/codebuild_eks_role.pol.tpl")
+#
+#  tags = merge(
+#    {
+#      "Name" = "${module.generator.prefix}-codebuild-eks"
+#    },
+#    module.generator.common_tags
+#  )
+#}
+#
+#resource "aws_iam_role_policy_attachment" "eks_policy" {
+#  role       = aws_iam_role.eks_master.name
+#  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+#}
+#
+#resource "aws_iam_role_policy_attachment" "eks_controller_policy" {
+#  role       = aws_iam_role.eks_master.name
+#  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+#}
