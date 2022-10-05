cae5959a0ddfe4c783f059afc8eae60bb46b1bf7
Uladzislau Muraveika <uladzislau.muraveika@protofire.io>
2022-10-05 18:23:23 +0300

    [feat] - add secret for the new api-read-slave-3

--- k8s/secrets.tf
+++ k8s/secrets.tf
@@ -141,6 +141,18 @@ resource "kubernetes_secret_v1" "api_read_slave_2_mainnet_lotus_secret" {
   }
 }
 
+resource "kubernetes_secret_v1" "api_read_slave_3_mainnet_lotus_secret" {
+  count = local.is_mainnet_envs
+  metadata {
+    name      = "api-read-slave-3-lotus-secret"
+    namespace = kubernetes_namespace_v1.network.metadata[0].name
+  }
+  data = {
+    privatekey = lookup(jsondecode(data.aws_secretsmanager_secret_version.api_read_master_mainnet_lotus[0].secret_string), "private_key", null)
+    token      = lookup(jsondecode(data.aws_secretsmanager_secret_version.api_read_master_mainnet_lotus[0].secret_string), "jwt_token", null)
+  }
+}
+
 resource "kubernetes_secret_v1" "space00_mainnet_lotus_secret" {
   count = local.is_mainnet_envs
   metadata {
