f6b7120ad5db4486e03da94f632cc3f8d0047492
Arsenii Petrovich <arsenii@protofire.io>
2022-10-10 21:46:04 +0300

    change name for ingress backend services

--- k8s/modules-ingress-mainnet.tf
+++ k8s/modules-ingress-mainnet.tf
@@ -5,7 +5,7 @@ module "ingress-kong_cid-checker-mainnet" {
   get_global_configuration           = local.make_global_configuration
   get_ingress_http_path              = "/"
   get_ingress_pathType              = "Prefix"
-  get_ingress_backend_service_name   = "cid-checker-frontend" // the "-service" string will be added automatically
+  get_ingress_backend_service_name   = "cid-checker-mainnet-frontend" // the "-service" string will be added automatically
   get_ingress_backend_service_port   = 80
   get_ingress_namespace              = "default" 
   get_rule_host                      = "filecoin.tools"
@@ -19,7 +19,7 @@ module "ingress-kong_cid-checker-mainnet-api" {
   source                             = "../modules/k8s_ingress"
   get_global_configuration           = local.make_global_configuration
   get_ingress_http_path              = "/api/(.*)"
-  get_ingress_backend_service_name   = "cid-checker-backend" // the "-service" string will be added automatically
+  get_ingress_backend_service_name   = "cid-checker-mainnet-backend" // the "-service" string will be added automatically
   get_ingress_backend_service_port   = 3000
   get_ingress_namespace              = "default"
   get_rule_host                      = "filecoin.tools"
@@ -34,7 +34,7 @@ module "ingress-kong_cid-checker-alternative-domain-mainnet" {
   get_global_configuration           = local.make_global_configuration
   get_ingress_http_path              = "/"
   get_ingress_pathType              = "Prefix"
-  get_ingress_backend_service_name   = "cid-checker-frontend" // the "-service" string will be added automatically
+  get_ingress_backend_service_name   = "cid-checker-mainnet-frontend" // the "-service" string will be added automatically
   get_ingress_backend_service_port   = 80
   get_ingress_namespace              = "default"
   get_rule_host                      = "cid.filecoin.tools"
@@ -48,7 +48,7 @@ module "ingress-kong_cid-checker-alternative-domain-mainnet-api" {
   source                             = "../modules/k8s_ingress"
   get_global_configuration           = local.make_global_configuration
   get_ingress_http_path              = "/api/(.*)"
-  get_ingress_backend_service_name   = "cid-checker-backend" // the "-service" string will be added automatically
+  get_ingress_backend_service_name   = "cid-checker-mainnet-backend" // the "-service" string will be added automatically
   get_ingress_backend_service_port   = 3000
   get_ingress_namespace              = "default"
   get_rule_host                      = "cid.filecoin.tools"
