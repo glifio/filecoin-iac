c6598264ad5e9ada44cc10d6e29cfbdf11c21a67
Ales Dumikau <ales.dumikau@protofire.io>
2022-10-26 18:53:46 +0300

    Update dashboard, add service monitor, update monitoring service

--- k8s/kong_monitoring.tf
+++ k8s/kong_monitoring.tf
@@ -1,13 +1,59 @@
-resource "kubernetes_manifest" "kong_monitoring_plugin" {
+resource "kubernetes_manifest" "kong_monitoring_plugin_external" {
   manifest = {
     apiVersion = "configuration.konghq.com/v1"
     kind = "KongClusterPlugin"
     metadata = {
-        name = "${terraform.workspace}-kong-monitoring-plugin"
+        name = "${terraform.workspace}-kong-monitoring-plugin-external"
         labels = {
             global = "true"
+        }
+        annotations = {
+            "kubernetes.io/ingress.class" = "kong-external-lb"
         }
     }
     plugin = "prometheus"
   }
+}
+
+resource "kubernetes_manifest" "kong_monitoring_plugin_internal" {
+  manifest = {
+    apiVersion = "configuration.konghq.com/v1"
+    kind = "KongClusterPlugin"
+    metadata = {
+        name = "${terraform.workspace}-kong-monitoring-plugin-internal"
+        labels = {
+            global = "true"
+        }
+        annotations = {
+            "kubernetes.io/ingress.class" = "kong-internal-lb"
+        }
+    }
+    plugin = "prometheus"
+  }
+}
+
+
+
+resource "kubernetes_service" "kong_monitoring_service" {
+    metadata {
+        name = "${terraform.workspace}-kong-monitoring-service"
+        namespace = kubernetes_namespace_v1.kong.metadata[0].name
+        labels = {
+            app = "kong-monitoring-service"
+        }
+    }
+
+    spec {
+        selector = {
+            "app.kubernetes.io/name" = "kong"
+        }
+
+        port {
+            name = "metrics"
+            port = 8100
+            target_port = 8100
+        }
+
+        type = "ClusterIP"
+    }
 }
