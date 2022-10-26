2f29ddc526d1adb35c31c01a2abf529a8642fe37
Alexey_Kulik <alexey.kulik@protofire.io>
2022-10-26 11:03:10 +0400

    refactoring: reducing files count

--- k8s/modules_aws_snapshots_dev.tf
+++
@@ -1,9 +0,0 @@
-module "calibrationapi_archive_node_snapshot" {
-  count         = local.is_dev_envs
-  source        = "../modules/cronjob_aws_snapshots_managment"
-  sts_name      = "calibrationapi-archive-node"
-  namespace     = kubernetes_namespace_v1.network.metadata[0].name
-  creator_cron  = "0 0 * * SAT"
-  deleter_cron  = "0 2 * * SAT"
-  snaps_to_keep = 2
-}
