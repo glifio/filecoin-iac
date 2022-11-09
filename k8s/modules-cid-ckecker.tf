bddb92e336f4ee38ff46524de1ed0563ef4bdc0c
lazavikmaria <lazavikmaria@gmail.com>
2022-11-09 10:00:20 +0400

    Fixed bucket name.

--- k8s/modules-cid-ckecker.tf
+++ k8s/modules-cid-ckecker.tf
@@ -1,7 +1,7 @@
 module "marketdeals_calibration" {
   count                    = local.is_dev_envs
   source                   = "../modules/cid_checker"
-  bucket_name              = "calibration"
+  bucket_name              = "marketdeals-calibration"
   get_sa_namespace         = "default"
   get_global_configuration = local.make_global_configuration
 }
@@ -9,7 +9,7 @@ module "marketdeals_calibration" {
 module "marketdeals_mainnet" {
   count                    = local.is_mainnet_envs
   source                   = "../modules/cid_checker"
-  bucket_name              = "mainnet"
+  bucket_name              = "marketdeals"
   get_sa_namespace         = "default"
   get_global_configuration = local.make_global_configuration
 }
@@ -17,7 +17,7 @@ module "marketdeals_mainnet" {
 module "marketdeals_wallaby" {
   count                    = local.is_dev_envs
   source                   = "../modules/cid_checker"
-  bucket_name              = "wallaby"
+  bucket_name              = "marketdeals-wallaby"
   get_sa_namespace         = "default"
   get_global_configuration = local.make_global_configuration
 }
