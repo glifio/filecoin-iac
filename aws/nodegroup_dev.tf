4caa0f03ed93c4c1d32e8cf92616042d3ebdaae2
Ales Dumikau <ales.dumikau@protofire.io>
2022-11-16 12:59:55 +0300

    Clear dev cluster and fix minor bugs

--- aws/nodegroup_dev.tf
+++ aws/nodegroup_dev.tf
@@ -9,24 +9,6 @@ module "eks_nodegroup_ondemand_group1" {
   get_eks_nodegroups_global_configuration = local.make_eks_nodegroups_global_configuration
 }
 
-module "eks_nodegroup_ondemand_group3" {
-  count                                   = local.is_dev_envs
-  source                                  = "../modules/eks_nodegroup"
-  get_instance_type                       = "r5.2xlarge"
-  get_nodegroup_name                      = "group3" # don't need to type ondemand/spot in the name, it will be added automatically.
-  get_global_configuration                = local.make_global_configuration
-  get_eks_nodegroups_global_configuration = local.make_eks_nodegroups_global_configuration
-}
-
-module "eks_nodegroup_ondemand_group4" {
-  count                                   = local.is_dev_envs
-  source                                  = "../modules/eks_nodegroup"
-  get_instance_type                       = "r5.2xlarge"
-  get_nodegroup_name                      = "group4" # don't need to type ondemand/spot in the name, it will be added automatically.
-  get_global_configuration                = local.make_global_configuration
-  get_eks_nodegroups_global_configuration = local.make_eks_nodegroups_global_configuration
-}
-
 ################# END BLOCK ONDEMAND NODE-GROUP LIST #################
 
 
