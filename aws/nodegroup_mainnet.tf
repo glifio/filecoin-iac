dfc715819558295558cfbbde4a60ed32705701f7
lazavikmaria <lazavikmaria@gmail.com>
2022-11-26 15:12:08 +0400

    Add new instance types for group10 spot.

--- aws/nodegroup_mainnet.tf
+++ aws/nodegroup_mainnet.tf
@@ -120,7 +120,7 @@ module "eks_nodegroup_mainnet_spot_group9" {
 module "eks_nodegroup_mainnet_spot_group10" {
   count                                   = local.is_prod_envs
   source                                  = "../modules/eks_nodegroup"
-  get_instance_type                       = "i3.2xlarge"
+  get_instance_type                       = "r5ad.4xlarge,r5dn.4xlarge,r5d.2xlarge,r6id.2xlarge"
   get_nodegroup_name                      = "group10" # don't need to type ondemand/spot in the name, it will be added automatically.
   get_global_configuration                = local.make_global_configuration
   get_eks_nodegroups_global_configuration = local.make_eks_nodegroups_global_configuration
