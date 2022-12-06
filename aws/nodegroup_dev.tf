8d725ea21b6ca191f768ef1c04a8fe2c0d7b4138
lazavikmaria <lazavikmaria@gmail.com>
2022-12-06 17:22:37 +0400

    add ondemand graviton instance to mainnet.

--- aws/nodegroup_dev.tf
+++ aws/nodegroup_dev.tf
@@ -25,11 +25,11 @@ module "eks_nodegroup_spot_group2" {
 }
 
  #this group is for future spot tests
-# module "eks_nodegroup_spot_group5" {
+## module "eks_nodegroup_spot_group5" {
 #   count                                   = local.is_dev_envs
 #   source                                  = "../modules/eks_nodegroup"
 #   ami_type                                = "AL2_ARM_64"
-#   get_instance_type                       = "m6gd.8xlarge"
+#   get_instance_type                       = "r6gd.4xlarge"
 #   get_nodegroup_name                      = "group5" # don't need to type ondemand/spot in the name, it will be added automatically.
 #   get_global_configuration                = local.make_global_configuration
 #   get_eks_nodegroups_global_configuration = local.make_eks_nodegroups_global_configuration
