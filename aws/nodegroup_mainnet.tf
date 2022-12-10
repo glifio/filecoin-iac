a24e9a4f824d0783d0c31772c4f8f69aa45d0e6c
lazavikmaria <lazavikmaria@gmail.com>
2022-12-10 23:54:38 +0400

    add 17 nodegroup arm.

--- aws/nodegroup_mainnet.tf
+++ aws/nodegroup_mainnet.tf
@@ -136,6 +136,17 @@ module "eks_nodegroup_ondemand_group16" {
   get_eks_nodegroups_global_configuration = local.make_eks_nodegroups_global_configuration
 }
 
+module "eks_nodegroup_ondemand_group17" {
+  count                                   = local.is_prod_envs
+  source                                  = "../modules/eks_nodegroup"
+  ami_type                                = "AL2_ARM_64"
+  get_instance_type                       = "r6gd.4xlarge"
+  user_data_script                        = "nvme-spot.sh"
+  get_nodegroup_name                      = "group17" # don't need to type ondemand/spot in the name, it will be added automatically.
+  get_global_configuration                = local.make_global_configuration
+  get_eks_nodegroups_global_configuration = local.make_eks_nodegroups_global_configuration
+}
+
 ################# END BLOCK ONDEMAND NODE-GROUP LIST #################
 
 
