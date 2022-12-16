78caff270d995f81ce9a91c606c244544be77e08
Ales Dumikau <ales.dumikau@protofire.io>
2022-12-16 19:41:08 +0300

    Delete residual space00 dependencies

--- k8s/modules-snapshots.tf
+++ k8s/modules-snapshots.tf
@@ -25,16 +25,16 @@ module "calibrationapi_archive_node_snapshot" {
 ### Start block: Snapshot for mainnet ###
 
 ## A module space00_snapshot should create aws snapshots
-module "space00_snapshot" {
-  count              = local.is_prod_envs
-  source             = "../modules/cronjob_aws_snapshots_managment"
-  sts_name           = "space00"
-  volume_name_prefix = "vol-lotus-io2"
-  namespace          = kubernetes_namespace_v1.network.metadata[0].name
-  creator_cron       = "0 0 * * *"
-  deleter_cron       = "0 2 * * *"
-  snaps_to_keep      = 7
-}
+#module "space00_snapshot" {
+#  count              = local.is_prod_envs
+#  source             = "../modules/cronjob_aws_snapshots_managment"
+#  sts_name           = "space00"
+#  volume_name_prefix = "vol-lotus-io2"
+#  namespace          = kubernetes_namespace_v1.network.metadata[0].name
+#  creator_cron       = "0 0 * * *"
+#  deleter_cron       = "0 2 * * *"
+#  snaps_to_keep      = 7
+#}
 
 module "mainnet_chain_snapshot" {
   count        = local.is_prod_envs
