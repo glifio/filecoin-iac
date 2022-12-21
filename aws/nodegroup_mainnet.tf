f7b6416f3a2c9b90080e5c95d0b3c1988e57c445
Ales Dumikau <ales.dumikau@protofire.io>
2022-12-21 11:34:08 +0300

    Delete old space07 nodegroup

--- aws/nodegroup_mainnet.tf
+++ aws/nodegroup_mainnet.tf
@@ -31,14 +31,14 @@ module "eks_nodegroup_mainnet_ondemand_group3" {
 }
 
 #prod-ng-r5-8x-workers-ondemand-b-1-19-Node
-module "eks_nodegroup_mainnet_ondemand_group4" {
-  count                                   = local.is_prod_envs
-  source                                  = "../modules/eks_nodegroup"
-  get_instance_type                       = "r5b.12xlarge"
-  get_nodegroup_name                      = "group4" # don't need to type ondemand/spot in the name, it will be added automatically.
-  get_global_configuration                = local.make_global_configuration
-  get_eks_nodegroups_global_configuration = local.make_eks_nodegroups_global_configuration
-}
+#module "eks_nodegroup_mainnet_ondemand_group4" {
+#  count                                   = local.is_prod_envs
+#  source                                  = "../modules/eks_nodegroup"
+#  get_instance_type                       = "r5b.12xlarge"
+#  get_nodegroup_name                      = "group4" # don't need to type ondemand/spot in the name, it will be added automatically.
+#  get_global_configuration                = local.make_global_configuration
+#  get_eks_nodegroups_global_configuration = local.make_eks_nodegroups_global_configuration
+#}
 
 #prod-ng-r5-12x-workers-ondemand-a-1-19-Node
 #module "eks_nodegroup_mainnet_ondemand_group5" {
