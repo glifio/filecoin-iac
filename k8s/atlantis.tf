906940a08b13ec3edb1495b9e1d85134325cf208
lazavikmaria <lazavikmaria@gmail.com>
2023-01-18 16:37:33 +0400

    fix atlantis

--- k8s/atlantis.tf
+++ k8s/atlantis.tf
@@ -4,7 +4,6 @@ resource "helm_release" "atlantis" {
   chart      = "atlantis"
   version    = "4.10.0"
 
-
   set {
     name  = "github.user"
     value = lookup(jsondecode(data.aws_secretsmanager_secret_version.atlantis_github_token.secret_string), "user", null)
