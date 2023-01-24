22797d6b3cd73933b40a12880796554a8331c28e
lazavikmaria <lazavikmaria@gmail.com>
2023-01-24 14:31:31 +0400

    add atlantis

--- k8s/atlantis.tf
+++ k8s/atlantis.tf
@@ -1,15 +1,15 @@
-#resource "helm_release" "atlantis" {
-#  name       = "atlantis"
-#  repository = "https://runatlantis.github.io/helm-charts"
-#  chart      = "atlantis"
-#  version    = "4.10.0"
-#
-#  values = [
-#    templatefile("${path.module}/configs/atlantis/atlantis-values.yaml", {
-#      orgWhitelist  = "github.com/glifio/*"
-#      github_user   = lookup(jsondecode(data.aws_secretsmanager_secret_version.atlantis_github_token.secret_string), "user", null)
-#      github_token  = lookup(jsondecode(data.aws_secretsmanager_secret_version.atlantis_github_token.secret_string), "token", null)
-#      github_secret = lookup(jsondecode(data.aws_secretsmanager_secret_version.atlantis_github_token.secret_string), "secret", null)
-#    })
-#  ]
-#}
+resource "helm_release" "atlantis" {
+  name       = "atlantis"
+  repository = "https://runatlantis.github.io/helm-charts"
+  chart      = "atlantis"
+  version    = "4.10.0"
+
+  values = [
+    templatefile("${path.module}/configs/atlantis/atlantis-values.yaml", {
+      orgWhitelist  = "github.com/glifio/*"
+      github_user   = lookup(jsondecode(data.aws_secretsmanager_secret_version.atlantis_github_token.secret_string), "user", null)
+      github_token  = lookup(jsondecode(data.aws_secretsmanager_secret_version.atlantis_github_token.secret_string), "token", null)
+      github_secret = lookup(jsondecode(data.aws_secretsmanager_secret_version.atlantis_github_token.secret_string), "secret", null)
+    })
+  ]
+}
