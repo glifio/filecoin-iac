29d8168254e4b8da3d87eb864518058e022c1fb1
lazavikmaria <lazavikmaria@gmail.com>
2023-02-02 14:02:50 +0400

    add opeworklabbot-atlantis

--- users.tf
+++ users.tf
@@ -89,5 +89,14 @@ locals {
         "${local.eks_cluster_mainnet}" = "${local.codebuild_wallaby_role.eks_group}"
       },
     },
+    {
+      username = "openworklabbot-atlantis",
+      aws_account = [
+        "devops"
+      ],
+      eks_access = {
+        "${local.eks_cluster_mainnet}" = "${local.devops_role.eks_group}"
+      },
+    },
   ]
 }
