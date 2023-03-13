4352f320eaf915bc3725e24df8c5040a95fe2f31
lazavikmaria <115006984+lazavikmaria@users.noreply.github.com>
2023-03-13 17:50:01 +0400

    type = "http"

--- modules/uptimerobot/main.tf
+++ modules/uptimerobot/main.tf
@@ -1,7 +1,7 @@
 # Create a monitor
 resource "uptimerobot_monitor" "main" {
   friendly_name = "wallaby.node.glif.io"
-  type          = "https"
+  type          = "http"
   url           = "https://wallaby.node.glif.io/rpc/v0"
   interval      = "1"
 }
