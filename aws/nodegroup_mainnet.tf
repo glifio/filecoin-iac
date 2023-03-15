f027cbee3953d41c6e1f6b0d51f45b9310746bea
Ales Dumikau <ales.dumikau@altoros.com>
2023-03-15 12:27:33 +0300

    Add yet another 2 spot nodes

--- aws/nodegroup_mainnet.tf
+++ aws/nodegroup_mainnet.tf
@@ -241,4 +241,24 @@ module "eks_nodegroup_mainnet_spot_group22" {
   is_spot_instance                        = true
 }
 
+module "eks_nodegroup_mainnet_spot_group23" {
+  count                                   = local.is_prod_envs
+  source                                  = "../modules/eks_nodegroup"
+  get_instance_type                       = "m5d.8xlarge,r5ad.8xlarge"
+  get_nodegroup_name                      = "group23" # don't need to type ondemand/spot in the name, it will be added automatically.
+  get_global_configuration                = local.make_global_configuration
+  get_eks_nodegroups_global_configuration = local.make_eks_nodegroups_global_configuration
+  is_spot_instance                        = true
+}
+
+module "eks_nodegroup_mainnet_spot_group24" {
+  count                                   = local.is_prod_envs
+  source                                  = "../modules/eks_nodegroup"
+  get_instance_type                       = "m5d.8xlarge,r5ad.8xlarge"
+  get_nodegroup_name                      = "group24" # don't need to type ondemand/spot in the name, it will be added automatically.
+  get_global_configuration                = local.make_global_configuration
+  get_eks_nodegroups_global_configuration = local.make_eks_nodegroups_global_configuration
+  is_spot_instance                        = true
+}
+
 ################# END BLOCK SPOT NODE-GROUP LIST #################
