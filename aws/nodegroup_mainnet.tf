4915cec2243217e57f6ff2e37d98a39fef7798f0
dumikau <44052182+dumikau@users.noreply.github.com>
2023-03-15 11:48:48 +0300

    Add supporting spot nodes (#142)

--- aws/nodegroup_mainnet.tf
+++ aws/nodegroup_mainnet.tf
@@ -220,4 +220,25 @@ module "eks_nodegroup_mainnet_spot_group10" {
   is_spot_instance                        = true
 }
 
+# Temporary spot nodes for migration
+module "eks_nodegroup_mainnet_spot_group21" {
+  count                                   = local.is_prod_envs
+  source                                  = "../modules/eks_nodegroup"
+  get_instance_type                       = "m5d.8xlarge,r5ad.8xlarge"
+  get_nodegroup_name                      = "group21" # don't need to type ondemand/spot in the name, it will be added automatically.
+  get_global_configuration                = local.make_global_configuration
+  get_eks_nodegroups_global_configuration = local.make_eks_nodegroups_global_configuration
+  is_spot_instance                        = true
+}
+
+module "eks_nodegroup_mainnet_spot_group22" {
+  count                                   = local.is_prod_envs
+  source                                  = "../modules/eks_nodegroup"
+  get_instance_type                       = "m5d.8xlarge,r5ad.8xlarge"
+  get_nodegroup_name                      = "group22" # don't need to type ondemand/spot in the name, it will be added automatically.
+  get_global_configuration                = local.make_global_configuration
+  get_eks_nodegroups_global_configuration = local.make_eks_nodegroups_global_configuration
+  is_spot_instance                        = true
+}
+
 ################# END BLOCK SPOT NODE-GROUP LIST #################
