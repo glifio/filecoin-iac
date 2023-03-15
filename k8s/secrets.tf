fa4b495485b8fe15d97fe1ac9cc22dd29c7729df
Ales Dumikau <ales.dumikau@altoros.com>
2023-03-15 11:47:23 +0300

    Add supporting spot nodes

--- k8s/secrets.tf
+++ k8s/secrets.tf
@@ -239,6 +239,30 @@ resource "kubernetes_secret_v1" "api_read_slave_3_mainnet_lotus_secret" {
   }
 }
 
+resource "kubernetes_secret_v1" "api_read_slave_4_mainnet_lotus_secret" {
+  count = local.is_prod_envs
+  metadata {
+    name      = "api-read-slave-4-lotus-secret"
+    namespace = kubernetes_namespace_v1.network.metadata[0].name
+  }
+  data = {
+    privatekey = lookup(jsondecode(data.aws_secretsmanager_secret_version.api_read_master_mainnet_lotus[0].secret_string), "private_key", null)
+    token      = lookup(jsondecode(data.aws_secretsmanager_secret_version.api_read_master_mainnet_lotus[0].secret_string), "jwt_token", null)
+  }
+}
+
+resource "kubernetes_secret_v1" "api_read_slave_5_mainnet_lotus_secret" {
+  count = local.is_prod_envs
+  metadata {
+    name      = "api-read-slave-5-lotus-secret"
+    namespace = kubernetes_namespace_v1.network.metadata[0].name
+  }
+  data = {
+    privatekey = lookup(jsondecode(data.aws_secretsmanager_secret_version.api_read_master_mainnet_lotus[0].secret_string), "private_key", null)
+    token      = lookup(jsondecode(data.aws_secretsmanager_secret_version.api_read_master_mainnet_lotus[0].secret_string), "jwt_token", null)
+  }
+}
+
 resource "kubernetes_secret_v1" "space00_mainnet_lotus_secret" {
   count = local.is_prod_envs
   metadata {
