2f76eeb8f9ff34094fbc7c8ab92a2b938868a40d
Ales Dumikau <ales.dumikau@altoros.com>
2023-03-20 11:51:19 +0300

    Fix body forwarding

--- k8s/kong_plugins/http_mirror/access.lua
+++ k8s/kong_plugins/http_mirror/access.lua
@@ -1,36 +1,14 @@
-local socket = require "socket"
-
 local http = require "socket.http"
-local ltn12 = require "ltn12"
-
-local get_path = kong.request.get_path
-local get_raw_body = kong.request.get_raw_body
 
 local _M = {}
 
 local function mirror(conf)
-    local path = get_path()
-    local body = get_raw_body()
-
-    kong.log(body)
+    local path = kong.request.get_path()
+    local body = kong.request.get_raw_body()
 
     if conf.mirror_to and string.len(body) > 0 then
         for _, value in ipairs(conf.mirror_to) do
-            local r, c, h, s = http.request{
-                method = "POST",
-                url = value .. path,
-                source = ltn12.source.string(body),
-                headers = {
-                    ["Content-Type"] = "application/json",
-                    ["Content-Length"] = string.len(body)
-                },
-                sink = nil,
-                create = function ()
-                    local req_sock = socket.tcp()
-                    req_sock:settimeout(0.05, 't')
-                    return req_sock
-                end
-            }
+            http.request(value .. path, body)
         end
     end
 end
