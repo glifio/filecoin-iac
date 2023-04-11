da118f0179372f774f7825d66d15649a1c421d16
lazavikmaria <lazavikmaria@gmail.com>
2023-04-11 12:27:24 +0400

    add some fix

--- aws/cloudfront_cache.tf
+++ aws/cloudfront_cache.tf
@@ -153,7 +153,7 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 
   default_cache_behavior {
     allowed_methods  = ["DELETE", "GET", "HEAD", "OPTIONS", "PATCH", "POST", "PUT"]
-    cached_methods   = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
     target_origin_id = local.s3_origin_id
 
     forwarded_values {
@@ -167,8 +167,8 @@ resource "aws_cloudfront_distribution" "s3_distribution" {
 
     viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
+    default_ttl            = 0
+    max_ttl                = 0
   }
 
   restrictions {
