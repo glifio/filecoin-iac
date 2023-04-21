a086620f3526515425ac66b320c0006138ff2481
lazavikmaria <lazavikmaria@gmail.com>
2023-04-21 09:37:04 +0400

    add nodegroup for space06

--- aws/nodegroup_mainnet.tf
+++ aws/nodegroup_mainnet.tf
@@ -205,16 +205,17 @@ module "eks_nodegroup_ondemand_group28" {
   get_eks_nodegroups_global_configuration = local.make_eks_nodegroups_global_configuration
 }
 
-#module "eks_nodegroup_ondemand_group29" {
-#  count                                   = local.is_prod_envs
-#  source                                  = "../modules/eks_nodegroup"
-#  ami_type                                = "AL2_ARM_64"
-#  get_instance_type                       = "r6gd.4xlarge"
-#  user_data_script                        = "nvme-spot.sh"
-#  get_nodegroup_name                      = "group29" # don't need to type ondemand/spot in the name, it will be added automatically.
-#  get_global_configuration                = local.make_global_configuration
-#  get_eks_nodegroups_global_configuration = local.make_eks_nodegroups_global_configuration
-#}
+module "eks_nodegroup_ondemand_group29" {
+  count                                   = local.is_prod_envs
+  source                                  = "../modules/eks_nodegroup"
+  ami_type                                = "AL2_ARM_64"
+  get_instance_type                       = "r6gd.4xlarge"
+  user_data_script                        = "nvme-spot.sh"
+  get_nodegroup_name                      = "group29" # don't need to type ondemand/spot in the name, it will be added automatically.
+  get_global_configuration                = local.make_global_configuration
+  get_eks_nodegroups_global_configuration = local.make_eks_nodegroups_global_configuration
+  assign_to_space00_07_nodes              = true
+}
 
 module "eks_nodegroup_ondemand_group34" {
   count                                   = local.is_prod_envs
