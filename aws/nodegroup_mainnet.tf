ba6960b5db4b3ca1feef20a9c65ef98d51cc794a
lazavikmaria <115006984+lazavikmaria@users.noreply.github.com>
2023-05-05 18:41:45 +0400

    change instance type r6gd.12xlarge --> r6g.12xlarge (#167)

--- aws/nodegroup_mainnet.tf
+++ aws/nodegroup_mainnet.tf
@@ -117,7 +117,7 @@ module "eks_nodegroup_ondemand_group15" {
   count                                   = local.is_prod_envs
   source                                  = "../modules/eks_nodegroup"
   ami_type                                = "AL2_ARM_64"
-  get_instance_type                       = "r6gd.12xlarge"
+  get_instance_type                       = "r6g.12xlarge"
   get_nodegroup_name                      = "group15" # don't need to type ondemand/spot in the name, it will be added automatically.
   get_global_configuration                = local.make_global_configuration
   get_eks_nodegroups_global_configuration = local.make_eks_nodegroups_global_configuration
@@ -163,7 +163,7 @@ module "eks_nodegroup_ondemand_group19" {
   count                                   = local.is_prod_envs
   source                                  = "../modules/eks_nodegroup"
   ami_type                                = "AL2_ARM_64"
-  get_instance_type                       = "r6gd.12xlarge"
+  get_instance_type                       = "r6g.12xlarge"
   get_nodegroup_name                      = "group19" # don't need to type ondemand/spot in the name, it will be added automatically.
   get_global_configuration                = local.make_global_configuration
   get_eks_nodegroups_global_configuration = local.make_eks_nodegroups_global_configuration
@@ -232,7 +232,7 @@ module "eks_nodegroup_ondemand_fvm_archive" {
   count                                   = local.is_prod_envs
   source                                  = "../modules/eks_nodegroup"
   ami_type                                = "AL2_ARM_64"
-  get_instance_type                       = "r6gd.12xlarge"
+  get_instance_type                       = "r6g.12xlarge"
   get_nodegroup_name                      = "fvm-archive" # don't need to type ondemand/spot in the name, it will be added automatically.
   get_global_configuration                = local.make_global_configuration
   get_eks_nodegroups_global_configuration = local.make_eks_nodegroups_global_configuration
@@ -257,15 +257,15 @@ module "eks_nodegroup_mainnet_spot_group8" {
 }
 
 ##prod-api-i3-4x8x-spot-c-1-19-Node
-#module "eks_nodegroup_mainnet_spot_group9" {
-#  count                                   = local.is_prod_envs
-#  source                                  = "../modules/eks_nodegroup"
-#  get_instance_type                       = "m5d.8xlarge,r5ad.8xlarge"
-#  get_nodegroup_name                      = "group9" # don't need to type ondemand/spot in the name, it will be added automatically.
-#  get_global_configuration                = local.make_global_configuration
-#  get_eks_nodegroups_global_configuration = local.make_eks_nodegroups_global_configuration
-#  is_spot_instance                        = true
-#}
+module "eks_nodegroup_mainnet_spot_group9" {
+  count                                   = local.is_prod_envs
+  source                                  = "../modules/eks_nodegroup"
+  get_instance_type                       = "m5d.8xlarge,r5ad.8xlarge"
+  get_nodegroup_name                      = "group9" # don't need to type ondemand/spot in the name, it will be added automatically.
+  get_global_configuration                = local.make_global_configuration
+  get_eks_nodegroups_global_configuration = local.make_eks_nodegroups_global_configuration
+  is_spot_instance                        = true
+}
 
 #prod-space06-1-i3-2x4x-spot-a-1-19-Node
 module "eks_nodegroup_mainnet_spot_group10" {
