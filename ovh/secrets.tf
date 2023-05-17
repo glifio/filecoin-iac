0499d7f6fdedc05d5474af23d1f9db6b4a5fbc24
Ales Dumikau <ales.dumikau@altoros.com>
2023-05-17 21:26:10 +0300

    Add secret for spacenet-slave-0

--- ovh/secrets.tf
+++ ovh/secrets.tf
@@ -5,3 +5,12 @@ module "spacenet_secret" {
 
   generator_config = local.generator_config
 }
+
+module "spacenet_slave_0_secret" {
+  source = "../modules/secrets_generator"
+
+  name        = "spacenet-public-slave-0"
+  from_secret = module.spacenet_secret.aws_secret_name
+
+  generator_config = local.generator_config
+}
