8365776366401302601cda76c14ff536ab337e5f
Ales Dumikau <ales.dumikau@altoros.com>
2023-05-17 21:25:41 +0300

    Add feature to import secret_string from another secret

--- modules/secrets_generator/data.tf
+++ modules/secrets_generator/data.tf
@@ -1,3 +1,13 @@
 data "external" "secret" {
   program = ["python3", "${path.module}/scripts/generator.py"]
 }
+
+data "aws_secretsmanager_secret" "from" {
+  count = local.from_secret_count
+  name  = local.from_secret_name
+}
+
+data "aws_secretsmanager_secret_version" "from" {
+  count     = local.from_secret_count
+  secret_id = data.aws_secretsmanager_secret.from[0].id
+}
