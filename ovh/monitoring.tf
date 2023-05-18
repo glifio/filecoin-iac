24e842d63b09e9ca03b30f887833be0a7aee368e
Ales Dumikau <ales.dumikau@altoros.com>
2023-05-18 16:29:31 +0300

    Split monitoring config to separate files

--- ovh/monitoring.tf
+++ ovh/monitoring.tf
@@ -5,12 +5,26 @@ resource "helm_release" "monitoring" {
   namespace  = kubernetes_namespace_v1.monitoring.metadata[0].name
   version    = "36.6.2"
 
-  values = [templatefile("${path.module}/config/prometheus/values.yaml", {
-    slack_channel = local.monitoring.slack.channel,
-    slack_api_url = local.monitoring.slack.api_url,
-    domain_name   = local.monitoring.domain_name,
-    pvc_size      = local.monitoring.pvc_size
-  })]
+  values = [
+    file("${path.module}/config/prometheus/general.yaml"),
+    file("${path.module}/config/prometheus/dashboards.yaml"),
+    file("${path.module}/config/prometheus/monitors.yaml"),
+    file("${path.module}/config/prometheus/notification-template.yaml"),
+
+    templatefile("${path.module}/config/prometheus/prometheus.yaml", {
+      pvc_size = local.monitoring.pvc_size
+    }),
+
+    templatefile("${path.module}/config/prometheus/grafana.yaml", {
+      domain_name   = local.monitoring.domain_name,
+      slack_api_url = local.monitoring.slack.api_url
+    }),
+
+    templatefile("${path.module}/config/prometheus/alert-manager.yaml", {
+      slack_api_url = local.monitoring.slack.api_url,
+      slack_channel = local.monitoring.slack.channel
+    })
+  ]
 
   set {
     name  = "grafana.adminPassword"
