2baa877ed4e5cedad8bb3cd94211c549cbd68ad2
Ales Dumikau <ales.dumikau@altoros.com>
2023-05-18 11:13:55 +0300

    Remove spacenet ingress

--- ovh/ingresses.tf
+++ ovh/ingresses.tf
@@ -1,20 +1,3 @@
-module "ingress_spacenet_gateway" {
-  count = local.production
-
-  name   = "spacenet-gateway"
-  source = "../modules/ovh_ingress"
-
-  http_host      = "api.spacenet.node.glif.io"
-  http_path      = "/"
-  http_path_type = "Prefix"
-
-  service_name = "spacenet-public-lotus-service"
-  service_port = 2346
-
-  enable_public_access = true
-  secret_name          = "${module.generator.prefix}-spacenet-public"
-}
-
 module "ingress_monitoring" {
   name   = "monitoring"
   source = "../modules/ovh_ingress"
