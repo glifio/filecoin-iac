658e7ea70ae3ec527c676131214932074788528f
dumikau <44052182+dumikau@users.noreply.github.com>
2023-05-25 17:04:19 +0300

    Make letsencrypt optional (#175)

--- modules/ovh_ingress/ingress.tf
+++ modules/ovh_ingress/ingress.tf
@@ -1,4 +1,6 @@
-resource "kubernetes_ingress_v1" "default" {
+resource "kubernetes_ingress_v1" "default_ssl" {
+  count = var.enable_letsencrypt ? 1 : 0
+
   metadata {
     name      = var.name
     namespace = var.namespace
@@ -17,6 +19,42 @@ resource "kubernetes_ingress_v1" "default" {
       hosts       = [var.http_host]
       secret_name = "${var.name}-ssl"
     }
+
+    rule {
+      host = var.http_host
+      http {
+        path {
+          path      = var.http_path
+          path_type = var.http_path_type
+          backend {
+            service {
+              name = var.service_name
+              port {
+                number = var.service_port
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_ingress_v1" "default" {
+  count = !var.enable_letsencrypt ? 1 : 0
+
+  metadata {
+    name      = var.name
+    namespace = var.namespace
+
+    annotations = {
+      "konghq.com/plugins"   = local.plugins_string
+      "konghq.com/protocols" = "https, http"
+    }
+  }
+
+  spec {
+    ingress_class_name = var.incress_class
 
     rule {
       host = var.http_host
