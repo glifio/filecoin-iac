30b37d6d22aff4f39d59543884e295da24970cca
Ales Dumikau <ales.dumikau@altoros.com>
2023-05-31 17:07:11 +0300

    Add node to YugabyteDB

--- aws/nodegroup_dev.tf
+++ aws/nodegroup_dev.tf
@@ -9,6 +9,21 @@ module "eks_nodegroup_ondemand_group1" {
   get_eks_nodegroups_global_configuration = local.make_eks_nodegroups_global_configuration
 }
 
+module "eks_nodegroup_ondemand_api_read_dev_db" {
+  count                                   = local.is_dev_envs
+  source                                  = "../modules/eks_nodegroup"
+  get_instance_type                       = "m5.4xlarge"
+  user_data_script                        = "yugabutedb.sh"
+  get_nodegroup_name                      = "api-read-dev-db" # don't need to type ondemand/spot in the name, it will be added automatically.
+  get_global_configuration                = local.make_global_configuration
+  get_eks_nodegroups_global_configuration = local.make_eks_nodegroups_global_configuration
+
+  custom_labels = {
+    app = "api-read-dev"
+    purpose = "db"
+  }
+}
+
 ################# END BLOCK ONDEMAND NODE-GROUP LIST #################
 
 
