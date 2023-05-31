4bcaa326dcbdce1d24b888ba099e7befcdd2a6f5
Ales Dumikau <ales.dumikau@altoros.com>
2023-05-31 17:05:09 +0300

    Add YugabyteDB helm release

---
+++ k8s/yugabutedb.tf
@@ -0,0 +1,24 @@
+resource "helm_release" "yugabutedb-api_read_dev" {
+  name = "api-read-dev-db"
+  namespace = "network"
+
+  repository = "https://charts.yugabyte.com"
+  chart = "yugabyte"
+  version = "2.17.3"
+
+  values = [
+    file("${path.module}/configs/yugabytedb/values.yaml")
+  ]
+
+  set {
+    name = "nodeSelector.app"
+    value = "api-read-dev"
+  }
+
+  set {
+    name = "nodeSelecor.purpose"
+    value = "db"
+  }
+
+  max_history = 2
+}
---
+++ modules/eks_nodegroup/bootstrap/early-customizations/yugabutedb.sh
@@ -0,0 +1,39 @@
+Content-Type: multipart/mixed; boundary="//"
+MIME-Version: 1.0
+
+--//
+Content-Type: text/cloud-config; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="cloud-config.txt"
+
+#cloud-config
+cloud_final_modules:
+- [scripts-user, always]
+
+--//
+Content-Type: text/x-shellscript; charset="us-ascii"
+MIME-Version: 1.0
+Content-Transfer-Encoding: 7bit
+Content-Disposition: attachment; filename="userdata.txt"
+
+#!/bin/bash
+
+# Recommended ulimits for YugabyteDB
+read -r -d '' ULIMITS <<- EOM
+*                -       core            unlimited
+*                -       data            unlimited
+*                -       fsize           unlimited
+*                -       sigpending      119934
+*                -       memlock         64
+*                -       rss             unlimited
+*                -       nofile          1048576
+*                -       msgqueue        819200
+*                -       stack           8192
+*                -       cpu             unlimited
+*                -       nproc           12000
+*                -       locks           unlimited
+EOM
+
+# Put ulimits to the dedicated file
+echo "$ULIMITS" > /etc/security/limits.d/yugabytedb.conf;
