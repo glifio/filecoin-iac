13ab23d75b899bdc335ccb86d7d26190b5ce2ad0
dumikau <44052182+dumikau@users.noreply.github.com>
2023-06-30 18:47:59 +0300

    Rotate credentials (#195)

--- ovh/secrets.tf
+++ ovh/secrets.tf
@@ -1,7 +1,7 @@
 module "spacenet_secret" {
   source = "../modules/secrets_generator"
 
-  name = "spacenet-public"
+  name = "spacenet-public-lotus"
 
   generator_config = local.generator_config
 }
@@ -9,7 +9,7 @@ module "spacenet_secret" {
 module "spacenet_slave_0_secret" {
   source = "../modules/secrets_generator"
 
-  name        = "spacenet-public-slave-0"
+  name        = "spacenet-public-slave-0-lotus"
   from_secret = module.spacenet_secret.aws_secret_name
 
   generator_config = local.generator_config
