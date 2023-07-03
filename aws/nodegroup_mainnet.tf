7bf356e6c3efbab4863420989b2292a4f304cec7
Arsenii Petrovich <arsenii@protofire.io>
2023-07-03 19:52:19 +0300

    shutdown blockscout-1

--- aws/nodegroup_mainnet.tf
+++ aws/nodegroup_mainnet.tf
@@ -175,15 +175,15 @@ module "eks_nodegroup_spot_calibnet_1" {
   is_spot_instance                        = true
 }
 
-module "eks_nodegroup_spot_blockscout_1" {
-  count                                   = local.is_prod_envs
-  source                                  = "../modules/eks_nodegroup"
-  get_instance_type                       = "m5d.8xlarge,r5ad.8xlarge"
-  get_nodegroup_name                      = "blockscout-1" # don't need to type ondemand/spot in the name, it will be added automatically.
-  get_global_configuration                = local.make_global_configuration
-  get_eks_nodegroups_global_configuration = local.make_eks_nodegroups_global_configuration
-  is_spot_instance                        = true
-}
+#module "eks_nodegroup_spot_blockscout_1" {
+#  count                                   = local.is_prod_envs
+#  source                                  = "../modules/eks_nodegroup"
+#  get_instance_type                       = "m5d.8xlarge,r5ad.8xlarge"
+#  get_nodegroup_name                      = "blockscout-1" # don't need to type ondemand/spot in the name, it will be added automatically.
+#  get_global_configuration                = local.make_global_configuration
+#  get_eks_nodegroups_global_configuration = local.make_eks_nodegroups_global_configuration
+#  is_spot_instance                        = true
+#}
 
 module "eks_nodegroup_ondemand_confirm_1" {
   count                                   = local.is_prod_envs
