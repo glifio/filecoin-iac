c5ab27ebb0d1ddb0c9a13b403681657ac84d3842
lazavikmaria <lazavikmaria@gmail.com>
2023-07-05 17:03:41 +0400

    switch from c2-60 to c2-15 instance type

--- ovh/secrets.tf
+++ ovh/secrets.tf
@@ -14,21 +14,3 @@ module "spacenet_slave_0_secret" {
 
   generator_config = local.generator_config
 }
-
-module "spacenet_slave_1_secret" {
-  source = "../modules/secrets_generator"
-
-  name        = "spacenet-public-slave-1-lotus"
-  from_secret = module.spacenet_secret.aws_secret_name
-
-  generator_config = local.generator_config
-}
-
-module "spacenet_slave_2_secret" {
-  source = "../modules/secrets_generator"
-
-  name        = "spacenet-public-slave-2-lotus"
-  from_secret = module.spacenet_secret.aws_secret_name
-
-  generator_config = local.generator_config
-}
