8b1febab5fb8644e5a025b87ba01e1a97214b220
lazavikmaria <lazavikmaria@gmail.com>
2023-07-07 18:24:33 +0400

    rotated instance type

--- aws/nodegroup_mainnet.tf
+++ aws/nodegroup_mainnet.tf
@@ -14,7 +14,7 @@ module "eks_nodegroup_mainnet_ondemand_group1" {
 module "eks_nodegroup_mainnet_ondemand_group6" {
   count                                   = local.is_prod_envs
   source                                  = "../modules/eks_nodegroup"
-  get_instance_type                       = "i3.4xlarge"
+  get_instance_type                       = "r5ad.2xlarge"
   get_nodegroup_name                      = "group6" # don't need to type ondemand/spot in the name, it will be added automatically.
   get_global_configuration                = local.make_global_configuration
   get_eks_nodegroups_global_configuration = local.make_eks_nodegroups_global_configuration
@@ -143,7 +143,7 @@ module "eks_nodegroup_ondemand_calibnet_0" {
   count                                   = local.is_prod_envs
   source                                  = "../modules/eks_nodegroup"
   ami_type                                = "AL2_ARM_64"
-  get_instance_type                       = "r6gd.4xlarge"
+  get_instance_type                       = "c6g.4xlarge"
   user_data_script                        = "nvme-spot.sh"
   get_nodegroup_name                      = "calibnet-0" # don't need to type ondemand/spot in the name, it will be added automatically.
   get_global_configuration                = local.make_global_configuration
@@ -226,6 +226,16 @@ module "eks_nodegroup_mainnet_spot_group10" {
   source                                  = "../modules/eks_nodegroup"
   get_instance_type                       = "m5d.8xlarge,r5ad.8xlarge"
   get_nodegroup_name                      = "group10" # don't need to type ondemand/spot in the name, it will be added automatically.
+  get_global_configuration                = local.make_global_configuration
+  get_eks_nodegroups_global_configuration = local.make_eks_nodegroups_global_configuration
+  is_spot_instance                        = true
+}
+
+module "eks_nodegroup_cid_checker_spot" {
+  count                                   = local.is_prod_envs
+  source                                  = "../modules/eks_nodegroup"
+  get_instance_type                       = "r5ad.4xlarge,r5a.4xlarge"
+  get_nodegroup_name                      = "cid_checker" # don't need to type ondemand/spot in the name, it will be added automatically.
   get_global_configuration                = local.make_global_configuration
   get_eks_nodegroups_global_configuration = local.make_eks_nodegroups_global_configuration
   is_spot_instance                        = true
