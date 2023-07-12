dcd914ecd1e26d87b4cbae363956f222f5f44e5f
lazavikmaria <lazavikmaria@gmail.com>
2023-07-12 13:30:27 +0400

    fixed slave-1

--- aws/nodegroup_mainnet.tf
+++ aws/nodegroup_mainnet.tf
@@ -203,8 +203,7 @@ module "eks_nodegroup_spot_calibnet_1" {
 module "eks_nodegroup_mainnet_spot_group8" {
   count                                   = local.is_prod_envs
   source                                  = "../modules/eks_nodegroup"
-  ami_type                                = "AL2_ARM_64"
-  get_instance_type                       = "r6gd.4xlarge" #m5d.8xlarge,r5ad.8xlarge for amd
+  get_instance_type                       = "m5d.8xlarge,r5ad.8xlarge"
   get_nodegroup_name                      = "group8" # don't need to type ondemand/spot in the name, it will be added automatically.
   get_global_configuration                = local.make_global_configuration
   get_eks_nodegroups_global_configuration = local.make_eks_nodegroups_global_configuration
