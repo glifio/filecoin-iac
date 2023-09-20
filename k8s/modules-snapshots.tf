9d43407796ba370298e4ca98027b00e8b6bac36b
Ales Dumikau <ales.dumikau@altoros.com>
2023-09-20 15:31:12 +0300

    Replace chain snapshot of space00 with spce07 one

--- k8s/modules-snapshots.tf
+++ k8s/modules-snapshots.tf
@@ -22,7 +22,7 @@ module "calibrationapi_archive_node_snapshot" {
 module "mainnet_chain_snapshot" {
   count        = local.is_prod_envs
   source       = "../modules/cronjob_lotus_chain_snapshots"
-  sts_name     = "space00"
+  sts_name     = "space07"
   namespace    = kubernetes_namespace_v1.network.metadata[0].name
   git_repo     = "git@gist.github.com:d03393d1f6e70e089e9e8d18922474f6.git"
   schedule     = "0 12 * * *"
