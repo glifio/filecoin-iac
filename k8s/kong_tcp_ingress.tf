7c98569432439cd321841d02630590ecd91ef029
Ales Dumikau <ales.dumikau@altoros.com>
2023-09-20 15:20:11 +0300

    Adjust TCP ports

--- k8s/kong_tcp_ingress.tf
+++ k8s/kong_tcp_ingress.tf
@@ -4,14 +4,14 @@
 #   proxy.stream[0].protocol
 # Warning: you need to place your TCPIngress CRD into the same namespace with the target service!
 
-resource "kubernetes_manifest" "kong_tcp_ingress" {
+resource "kubernetes_manifest" "kong_tcp_ingress_space07" {
   count = local.is_prod_envs
   manifest = {
     apiVersion = "configuration.konghq.com/v1beta1"
     kind       = "TCPIngress"
 
     metadata = {
-      name      = "${terraform.workspace}-space00-1235"
+      name      = "${terraform.workspace}-space07-1235"
       namespace = kubernetes_namespace_v1.network.metadata[0].name
       annotations = {
         "kubernetes.io/ingress.class" : "kong-external-lb"
@@ -21,34 +21,8 @@ resource "kubernetes_manifest" "kong_tcp_ingress" {
       rules = [{
         port = 1235
         backend = {
-          serviceName = "space00-lotus-service"
-          servicePort = 1235
-        }
-      }]
-    }
-  }
-}
-
-
-resource "kubernetes_manifest" "kong_tcp_ingress_space07" {
-  count = local.is_prod_envs
-  manifest = {
-    apiVersion = "configuration.konghq.com/v1beta1"
-    kind       = "TCPIngress"
-
-    metadata = {
-      name      = "${terraform.workspace}-space07-1236"
-      namespace = kubernetes_namespace_v1.network.metadata[0].name
-      annotations = {
-        "kubernetes.io/ingress.class" : "kong-external-lb"
-      }
-    }
-    spec = {
-      rules = [{
-        port = 1236
-        backend = {
           serviceName = "space07-lotus-service"
-          servicePort = 1236
+          servicePort = 1235
         }
       }]
     }
