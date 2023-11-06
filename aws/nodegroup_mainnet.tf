5bbd2677023f1c60b59cd442d5ddac6b5a95790e
lazavikmaria <115006984+lazavikmaria@users.noreply.github.com>
2023-11-06 16:49:06 +0100

    was changed r6gd.4xlarge-->r6gd.8xlarge for space06-1

--- aws/nodegroup_mainnet.tf
+++ aws/nodegroup_mainnet.tf
@@ -214,7 +214,7 @@ module "eks_nodegroup_mainnet_spot_group10" {
   source = "../modules/eks_nodegroup"
 
   name             = "group10"
-  instance_type    = "r6gd.4xlarge"
+  instance_type    = "r6gd.8xlarge"
   ami_type         = "AL2_ARM_64"
   is_spot_instance = true
 
