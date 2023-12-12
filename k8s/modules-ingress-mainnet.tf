578c2295df574e8e974a0d2442342b611318aea6
Ales Dumikau <ales.dumikau@altoros.com>
2023-12-12 23:54:33 +0300

    Revert "Switch traffic: space07 -> api-read-master, coinfirm -> api-read-master, fvm-archive -> api-read-master"
    This reverts commit bfe491a9aeedad92cd0313a82015cc17df9173a2.

--- k8s/modules-ingress-mainnet.tf
+++ k8s/modules-ingress-mainnet.tf
@@ -267,15 +267,17 @@ module "ingress_fvm_archive" {
   http_host = "node.glif.io"
   http_path = "/fvm-archive/lotus/(.*)"
 
-  service_name  = "api-read-master-lotus-service"
-  service_port  = 1234
+  service_name = "fvm-archive-lotus-service"
+  service_port = 1234
+
   incress_class = "kong-external-lb"
-  secret_name   = data.aws_secretsmanager_secret.api_read_master_mainnet_lotus[0].name
+
+  secret_name = data.aws_secretsmanager_secret.fvm_archive_lotus[0].name
 
   enable_path_transformer = true
   enable_access_control   = true
-  access_control_public   = true
-  access_control_replace  = true
+  access_control_public   = false
+  access_control_replace  = false
   enable_letsencrypt      = false
   enable_return_json      = true
 }
@@ -395,10 +397,12 @@ module "ingress_coinfirm" {
   http_host = "node.glif.io"
   http_path = "/coinfirm/lotus/(.*)"
 
-  service_name  = "api-read-master-lotus-service"
-  service_port  = 1234
+  service_name = "fvm-archive-lotus-service"
+  service_port = 1234
+
   incress_class = "kong-external-lb"
-  secret_name   = data.aws_secretsmanager_secret.api_read_master_mainnet_lotus[0].name
+
+  secret_name = data.aws_secretsmanager_secret.fvm_archive_lotus[0].name
 
   enable_path_transformer = true
   enable_access_control   = true
@@ -465,15 +469,11 @@ module "ingress_space07_1234" {
   http_path      = "/space07/lotus/(.*)"
   http_path_type = "Exact"
 
-  service_name  = "api-read-master-lotus-service"
+  service_name  = "space07-lotus-service"
   service_port  = 1234
   incress_class = "kong-external-lb"
-  secret_name   = data.aws_secretsmanager_secret.api_read_master_mainnet_lotus[0].name
+  secret_name   = data.aws_secretsmanager_secret.space07_mainnet_lotus[0].name
 
-  enable_path_transformer = true
-  enable_access_control   = true
-  access_control_public   = true
-  access_control_replace  = true
-  enable_letsencrypt      = false
-  enable_return_json      = true
+  enable_access_control = true
+  enable_letsencrypt    = false
 }
