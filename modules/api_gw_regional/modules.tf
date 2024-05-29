ed49ee4ee821ff047e18c862adc603ec1e66dbef
Ales Dumikau <ales.dumikau@protofire.io>
2024-05-29 11:31:53 +0300

    [chore] - cleanup unused resources

--- modules/api_gw_regional/modules.tf
+++
@@ -1,7 +0,0 @@
-module "generator" {
-  source          = "../../modules/generator"
-  project         = local.get_project
-  region          = local.get_region
-  environment     = local.get_environment
-  sub_environment = local.get_sub_environment
-}
