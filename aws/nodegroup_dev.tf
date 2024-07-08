5a10fbe614d7b4b40d6616c3c274d1912f6268a3
Ales Dumikau <ales.dumikau@protofire.io>
2024-07-08 16:57:03 +0300

    fix(nodes): extend api-read-dev root volume feat(nodes): add extra node named demo

--- aws/nodegroup_dev.tf
+++ aws/nodegroup_dev.tf
@@ -24,6 +24,19 @@ module "eks_nodegroup_spot_api_read_dev" {
   instance_type    = "r6gd.xlarge"
   ami_type         = "AL2_ARM_64"
   is_spot_instance = true
+  root_volume_size = 80
+
+  global_config    = local.make_global_configuration
+  nodegroup_config = local.make_eks_nodegroups_global_configuration
+}
+
+module "eks_nodegroup_dev_demo" {
+  count  = local.is_dev_envs
+  source = "../modules/eks_nodegroup"
+
+  name             = "demo" # don't need to type ondemand/spot in the name, it will be added automatically.
+  instance_type    = "c5a.2xlarge"
+  user_data        = "nvme-spot.sh"
 
   global_config    = local.make_global_configuration
   nodegroup_config = local.make_eks_nodegroups_global_configuration
