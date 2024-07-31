1f0f7e33d3c4faf43c9dad94ecf753961a429dfe
Ales Dumikau <ales.dumikau@protofire.io>
2024-07-31 12:55:58 +0300

    feat(nodes): add node for ltsh

--- aws/nodegroup_dev.tf
+++ aws/nodegroup_dev.tf
@@ -30,13 +30,27 @@ module "eks_nodegroup_spot_api_read_dev" {
   nodegroup_config = local.make_eks_nodegroups_global_configuration
 }
 
+module "eks_nodegroup_spot_api_read_dev_ltsh" {
+  count  = local.is_dev_envs
+  source = "../modules/eks_nodegroup"
+
+  name             = "api-read-dev-ltsh" # don't need to type ondemand/spot in the name, it will be added automatically.
+  instance_type    = "r6gd.xlarge"
+  ami_type         = "AL2_ARM_64"
+  is_spot_instance = true
+  root_volume_size = 80
+
+  global_config    = local.make_global_configuration
+  nodegroup_config = local.make_eks_nodegroups_global_configuration
+}
+
 module "eks_nodegroup_dev_demo" {
   count  = local.is_dev_envs
   source = "../modules/eks_nodegroup"
 
-  name             = "demo" # don't need to type ondemand/spot in the name, it will be added automatically.
-  instance_type    = "c5a.2xlarge"
-  user_data        = "nvme-spot.sh"
+  name          = "demo" # don't need to type ondemand/spot in the name, it will be added automatically.
+  instance_type = "c5a.2xlarge"
+  user_data     = "nvme-spot.sh"
 
   global_config    = local.make_global_configuration
   nodegroup_config = local.make_eks_nodegroups_global_configuration
