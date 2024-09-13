c99751d3859cc4585e2bb74f3277167d0bcfc3d7
Ales Dumikau <ales.dumikau@protofire.io>
2024-09-13 11:13:08 +0300

    feat(fvm-archive): enable query param token auth on fvm-archive endpoint

--- k8s/modules-ingress-mainnet.tf
+++ k8s/modules-ingress-mainnet.tf
@@ -279,6 +279,8 @@ module "ingress_fvm_archive" {
   access_control_replace  = true
   enable_return_json      = true
   enable_ext_token_auth   = true
+
+  enable_optional_query_param_auth = true
 }
 
 module "ingress_thegraph" {
