ea225218b6f90d5d672725b93f26215690c0014a
Ales Dumikau <ales.dumikau@protofire.io>
2024-10-03 18:17:46 +0300

    feat(ps): create proxy ingress

--- proteus_shield/locals.tf
+++ proteus_shield/locals.tf
@@ -13,4 +13,16 @@ locals {
     "${local.production_workspace}"  = "filecoin-mainnet-apn1-glif-eks"
   }
   kubernetes_cluster_name = local.kubernetes_cluster_name_config[terraform.workspace]
+
+  domain_zone_config = {
+    "${local.development_workspace}" = "dev.node.glif.io"
+    "${local.production_workspace}"  = "node.glif.io"
+  }
+  domain_zone = local.domain_zone_config[terraform.workspace]
+
+  elb_name_tag_config = {
+    "${local.development_workspace}" = "filecoin-dev-apn1-glif-kong-external"
+    "${local.production_workspace}"  = "filecoin-mainnet-apn1-glif-kong-external"
+  }
+  elb_name_tag = local.elb_name_tag_config[terraform.workspace]
 }
