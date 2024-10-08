62d9e9589bf2806e3f8bcc39d278406393171733
Ales Dumikau <ales.dumikau@protofire.io>
2024-10-08 23:01:21 +0300

    fix(thegraph): use hardcoded db_name
    The default db name is unused for some reason, and
    we use the secret value for helm deployment. So, it's
    hardcoded here and retreived from the secret in helm.

--- thegraph/rds.tf
+++ thegraph/rds.tf
@@ -1,6 +1,6 @@
 resource "aws_db_instance" "default" {
   allocated_storage   = var.rds_allocated_storage
-  db_name             = lookup(jsondecode(data.aws_secretsmanager_secret_version.rds_credentials.secret_string), "db_name", null)
+  db_name             = "thegraph"
   engine              = var.rds_engine
   engine_version      = var.rds_engine_version
   instance_class      = var.rds_instance_class
