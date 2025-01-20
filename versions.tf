40d9ba0c8b03d6a19cd6cd87ae20ff3aa089c9b7
Ales Dumikau <ales.dumikau@protofire.io>
2025-01-20 12:06:22 +0300

    chore(versions): upgrade terraform and kubernetes provider

--- versions.tf
+++ versions.tf
@@ -1,5 +1,5 @@
 terraform {
-  required_version = "1.3.1"
+  required_version = "1.5.0"
 
   required_providers {
     aws = {
@@ -12,7 +12,7 @@ terraform {
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "2.32.0"
+      version = "2.35.1"
     }
     helm = {
       source  = "hashicorp/helm"
@@ -21,10 +21,6 @@ terraform {
     tls = {
       source  = "hashicorp/tls"
       version = "4.0.2"
-    }
-    ovh = {
-      source  = "ovh/ovh"
-      version = "0.30.0"
     }
     local = {
       source  = "hashicorp/local"
